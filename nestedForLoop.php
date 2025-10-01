6b19631585f3dd69dca431d91b31a3cca939df8d
B00165442 Tobby Akinwale <B00165442@mytudublin.ie>
2025-10-01 10:36:55 +0100

    second part

---
+++ nestedForLoop.php
@@ -0,0 +1,15 @@
+<?PHP
+
+echo "<table border=1>";
+
+for($i=1;$i<=15;$i++){
+    echo "<tr>";
+    for($j=1;$j<=20;$j++){
+        echo "<td>".$i.",".$j."</td>";
+    }
+    echo "</tr>";
+}
+
+echo "</table>";
+
+?>
